ca016366e2a3d8554c97d19334368cc108691bec
Pal-cloud <paloma.gom.sal@gmail.com>
2026-01-15 12:35:28 +0100

    file models.py

--- libreria/libros/models.py
+++ libreria/libros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    fecha_publicacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
